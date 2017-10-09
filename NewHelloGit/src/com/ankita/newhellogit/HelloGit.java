8a70f1329cf99ec5d800a97c29c98229aa2797af
ankitasaxena <saxenaankita2010@gmail.com>
2017-10-09 12:15:39 -0400

    commited NewHelloGit project using the youtube video https://www.youtube.com/watch?v=cdsMIX9gB94

---
+++ NewHelloGit/src/com/ankita/newhellogit/HelloGit.java
@@ -0,0 +1,10 @@
+package com.ankita.newhellogit;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("testing the connection between eclipse nad git");
+
+	}
+
+}
